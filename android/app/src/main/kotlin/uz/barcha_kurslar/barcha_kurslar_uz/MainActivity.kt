53e10db16b7ef3992c604885b780b7aa13e00fde
Devituz <shohbozbek.uz24@gamil.com>
2025-07-01 03:02:55 -0700

    frfr

---
+++ android/app/src/main/kotlin/uz/barcha_kurslar/barcha_kurslar_uz/MainActivity.kt
@@ -0,0 +1,5 @@
+package uz.barcha_kurslar.barcha_kurslar_uz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'CustomCalendarPage.dart';
 
 void main() {
